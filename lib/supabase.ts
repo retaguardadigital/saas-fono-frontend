877fe0a913ec96fe8520dc4a35f092689b658d6f
Retaguarda Digital <contato@retaguarda.digital>
2026-04-13 19:22:59 +0200

    env supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  'https://vunrksemtwhwrglgcyjj.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1bnJrc2VtdHdod3JnbGdjeWpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwMDYwNTA'
+  process.env.NEXT_PUBLIC_https://vunrksemtwhwrglgcyjj.supabase.co!,
+  process.env.NEXT_PUBLIC_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1bnJrc2VtdHdod3JnbGdjeWpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwMDYwNTA
 );
