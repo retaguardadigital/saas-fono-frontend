bc7ab234e12d7ec02c0686134bc668986662f14f
Retaguarda Digital <contato@retaguarda.digital>
2026-04-13 19:35:35 +0200

    correção next

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_https://vunrksemtwhwrglgcyjj.supabase.co!,
-  process.env.NEXT_PUBLIC_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1bnJrc2VtdHdod3JnbGdjeWpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwMDYwNTA
+  process.env.NEXT_PUBLIC_SUPABASE_URL!https://vunrksemtwhwrglgcyjj.supabase.co!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1bnJrc2VtdHdod3JnbGdjeWpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwMDYwNTA
 );
